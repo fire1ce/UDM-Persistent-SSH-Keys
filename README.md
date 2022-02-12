231593868a3c7b81bf079f2e063a36c846b5942d
fire1ce <stas@3os.org>
2022-02-12 23:14:24 +0200

    initial release

--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 ## Persistence on Reboot
 
 The setup script must be run every time the system is rebooted since the __/root/.ssh/authorized_keys__ overwrites every boot.  
-This can be accomplished with a boot script. Flow its guide [UDM / UDMPro Boot Script]([UDM / UDMPro Boot Script](https://github.com/boostchicken-dev/udm-utilities/tree/master/on-boot-script)){target=_blank}
+This can be accomplished with a boot script. Flow its guide [UDM / UDMPro Boot Script]([UDM / UDMPro Boot Script](https://github.com/boostchicken-dev/udm-utilities/tree/master/on-boot-script))
+
+## Installation
+
+
+
+```bash
+```
--- install.sh
+++ install.sh
@@ -2,14 +2,18 @@
 
 user_authorized_keys_file="/mnt/data/ssh/authorized_keys"
 
-curl -O https://raw.githubusercontent.com/fire1ce/
+curl -O https://raw.githubusercontent.com/fire1ce/UDMP-Persistence-SSH-Keys/main/99-ssh-keys.sh
 mv 99-ssh-keys.sh /mnt/data/on_boot.d/99-ssh-keys.sh
 chmod +x /mnt/data/on_boot.d/99-ssh-keys.sh
+echo "99-ssh-keys.sh installed"
 
 if [ ! -f "$user_authorized_keys_file" ]; then
   touch $user_authorized_keys_file
+  echo "Creating $user_authorized_keys_file"
 fi
 
 chmod 0644 $user_authorized_keys_file
 
+echo "Add you public keys to $user_authorized_keys_file"
+echo "done."
 exit 0
